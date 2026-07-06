9a1aa166d7088a019eefa6cb872aa44d79aa7ea3
tsdesignltd <tsdesign.ltd@gmail.com>
2026-07-06 16:11:23 +0900

    Improve responsive catalog for large inventories

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "sunami-sale-v1";
-const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./icon.svg", "./manifest.webmanifest"];
+const CACHE = "sunami-sale-v2";
+const ASSETS = ["./", "./index.html", "./styles.css?v=2", "./app.js?v=2", "./icon.svg", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
