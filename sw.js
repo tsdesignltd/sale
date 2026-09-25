7130578164056bdcf36e82983698ba21dc06e70a
tsdesignltd <tsdesign.ltd@gmail.com>
2026-09-25 11:50:25 +0900

    Use portrait for product registration

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE = "sunami-sale-v43";
+const CACHE = "sunami-sale-v44";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=33",
+  "./styles.css?v=34",
   "./app.js?v=36",
   "./icon.svg",
   "./portrait.png",
